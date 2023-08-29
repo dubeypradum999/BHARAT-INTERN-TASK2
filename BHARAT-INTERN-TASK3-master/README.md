e1d1d6bba77f6ffa3e0d7e9b9c6b2f38df57268a
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2023-08-30 00:23:23 +0530

    Add files via upload

---
+++ BHARAT-INTERN-TASK3-master/README.md
@@ -0,0 +1,9 @@
+# BHARAT-INTERN-TASK3
+This is An App Development Project.
+In this project  I have created a quiz app.
+There are total 4 questions given.
+No of questions can be added more.
+After selecting the answers of all the above questions
+User will get their score.
+And there will be a prompt message  of their score.
+And after that restart button have been provided  
---
+++ BHARAT-INTERN-TASK3-master/app/src/main/java/com/example/myquiz/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.myquiz;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Full form of SQL?",
+            "Which company owns the Apple?"
+
+    };
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Phython"},
+            {"Structured Query Language","Start Question Language","Structure Query Line","Structured Query Live"},
+            {"Google","Apple","Nokia","Samsung"}
+
+    };
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Structured Query Language",
+            "Apple"
+
+    };
+
+
+
+
+}
